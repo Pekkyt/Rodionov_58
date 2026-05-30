f547ef8d19a80f096f16c7dbbfbec080f5809a4a
Rodion <rodionov.r26052006@gmail.com>
2026-05-30 22:36:03 +0300

    Декомпозировал функцию tokenize и обновил документацию

--- Rodionov_58/Rodionov_58.cpp
+++ Rodionov_58/Rodionov_58.cpp
@@ -15,171 +15,216 @@ using namespace std;
 static int NEXT_NODE_ID = 0;
 static int NEXT_ACTION_ID = 1;
 
-vector<Token> tokenize(const string& s, vector<Error>& errors) 
+void readInvalidNumberToken(const string& expression, int& currentPosition, int tokenStartPosition, vector<Error>& errors)
 {
-    // Создать пустой список токенов
-    vector<Token> t;
-    // Установить индекс текущего символа в начало строки
-    int i = 0;
-    // Пока индекс меньше длины строки
-    while (i < (int)s.size()) {
-
-        // Если текущий символ - пробел
-        if (isspace((unsigned char)s[i])) {
-
-            // Перейти к следующему символу
-            i++;
-            continue;
-        }
-        // Если текущий символ - цифра
-        if (isdigit((unsigned char)s[i]))
-        {
-
-            // Запомнить позицию начала числа
-            int st = i;
-            // Считать целую часть числа
-            while (i < (int)s.size() && isdigit((unsigned char)s[i])) { i++; }
-            // Если после целой части встречена точка
-            if (i < (int)s.size() && s[i] == '.')
-            {
-
-                // Добавить точку в число
-                i++;
-                // Проверить, что после точки следует хотя бы одна цифра
-                if (i >= (int)s.size() || !isdigit((unsigned char)s[i]))
-                {
-                    // Считать некорректное число целиком
-                    while (i < (int)s.size() && (isdigit((unsigned char)s[i]) || s[i] == '.' || s[i] == ',')) { i++; }
-                    // Если формат числа некорректен, добавить ошибку INVALID_NUMBER_FORMAT
-                    errors.push_back({ ErrorType::INVALID_NUMBER_FORMAT, st, s.substr(st, i - st) });
-                    continue;
-                }
-                // Считать дробную часть
-                while (i < (int)s.size() && isdigit((unsigned char)s[i])) { i++; }
-            }
-            // Если после числа снова встретилась точка или запятая, формат числа некорректен
-            if (i < (int)s.size() && (s[i] == '.' || s[i] == ','))
-            {
-                // Считать некорректное число целиком
-                while (i < (int)s.size() && (isdigit((unsigned char)s[i]) || s[i] == '.' || s[i] == ',')) { i++; }
-                // Если формат числа некорректен, добавить ошибку INVALID_NUMBER_FORMAT
-                errors.push_back({ ErrorType::INVALID_NUMBER_FORMAT, st, s.substr(st, i - st) });
-                continue;
-            }
-            // Получить строковое представление числа
-            string num = s.substr(st, i - st);
-            // Проверить, что модуль числа не превышает 2147483647
-            double value = stod(num);
-            // Если число выходит за диапазон, добавить ошибку NUMBER_OUT_OF_RANGE
-            if (fabs(value) > 2147483647.0)
-            {
-                errors.push_back({ ErrorType::NUMBER_OUT_OF_RANGE, st, num });
-                continue;
-            }
-            // Добавить токен числа в список.
-            t.push_back({ num, st, TokenType::NUMBER });
-            continue;
-        }
-        // Если число начинается с точки или запятой, формат числа некорректен
-        if (s[i] == '.' || s[i] == ',')
-        {
-            // Запомнить позицию начала некорректного числа
-            int st = i;
-            // Считать некорректное число целиком
-            while (i < (int)s.size() && (isdigit((unsigned char)s[i]) || s[i] == '.' || s[i] == ',')) { i++; }
-            // Добавить ошибку INVALID_NUMBER_FORMAT
-            errors.push_back({ ErrorType::INVALID_NUMBER_FORMAT, st, s.substr(st, i - st) });
-            continue;
-        }
-        // Если текущий символ - латинская буква
-        if ((unsigned char)s[i] < 128 && isalpha((unsigned char)s[i]))
-        {
-            // Запомнить позицию начала имени переменной
-            int st = i;
-            // Считать имя переменной целиком
-            while (i < (int)s.size() && (unsigned char)s[i] < 128 && isalnum((unsigned char)s[i])) { i++; }
-            // Добавить токен переменной в список
-            t.push_back({ s.substr(st, i - st), st, TokenType::VARIABLE });
-            continue;
-        }
-        // Если текущий символ - один из символов +, -, *, /, ^, (, )
-        if (string("+-*/^()").find(s[i]) != string::npos)
-        {
-            // Сохранить текст текущего символа
-            string cur(1, s[i]);
-            // По умолчанию считать токен оператором
-            TokenType type = TokenType::OPERATOR;
-            // Если символ является открывающей скобкой, задать тип LEFT_BRACKET
-            if (s[i] == '(') { type = TokenType::LEFT_BRACKET; }
-            // Если символ является закрывающей скобкой, задать тип RIGHT_BRACKET
-            else if (s[i] == ')') { type = TokenType::RIGHT_BRACKET; }
-            // Если символ является минусом, определить: унарный он или бинарный
-            else if (s[i] == '-')
-            {
-                // Если "-" находится в начале выражения, после "(" или после другого оператора, считать его унарным минусом
-                bool unary = t.empty() || t.back().type == TokenType::LEFT_BRACKET || t.back().type == TokenType::OPERATOR;
-                // Иначе считать символ "-" бинарной операцией вычитания
-                cur = unary ? "~" : "-";
-            }
-            // Добавить соответствующий токен в список.
-            t.push_back({ cur, i, type });
-            // Перейти к следующему символу.
-            i++;
-            continue;
-        }
-        // Если текущий символ не относится ни к одному допустимому случаю
-        // Добавить ошибку INVALID_SYMBOL
-        errors.push_back({ ErrorType::INVALID_SYMBOL, i, string(1, s[i]) });
-        // Перейти к следующему символу
-        i++;
+    // Считать некорректное число целиком
+    while (currentPosition < (int)expression.size() && (isdigit((unsigned char)expression[currentPosition]) || expression[currentPosition] == '.' || expression[currentPosition] == ','))
+    {
+        currentPosition++;
     }
-    // Если при разборе символов уже были ошибки, дополнительные проверки не выполнять
-    if (!errors.empty()) { return t; }
+    // Добавить ошибку INVALID_NUMBER_FORMAT
+    errors.push_back({ ErrorType::INVALID_NUMBER_FORMAT, tokenStartPosition, expression.substr(tokenStartPosition, currentPosition - tokenStartPosition) });
+}
+
+bool readNumberToken(const string& expression, int& currentPosition, vector<Token>& tokens, vector<Error>& errors)
+{
+    // Если текущий символ не является цифрой, число не считывается
+    if (!isdigit((unsigned char)expression[currentPosition]))
+    {
+        return false;
+    }
+    // Запомнить позицию начала числа
+    int tokenStartPosition = currentPosition;
+    // Считать целую часть числа
+    while (currentPosition < (int)expression.size() && isdigit((unsigned char)expression[currentPosition]))
+    {
+        currentPosition++;
+    }
+    // Если после целой части встречена точка
+    if (currentPosition < (int)expression.size() && expression[currentPosition] == '.')
+    {
+        // Добавить точку в число
+        currentPosition++;
+        // Проверить, что после точки следует хотя бы одна цифра
+        if (currentPosition >= (int)expression.size() || !isdigit((unsigned char)expression[currentPosition]))
+        {
+            readInvalidNumberToken(expression, currentPosition, tokenStartPosition, errors);
+            return true;
+        }
+        // Считать дробную часть числа
+        while (currentPosition < (int)expression.size() && isdigit((unsigned char)expression[currentPosition]))
+        {
+            currentPosition++;
+        }
+    }
+    // Если после числа снова встретилась точка или запятая, формат числа некорректен
+    if (currentPosition < (int)expression.size() && (expression[currentPosition] == '.' || expression[currentPosition] == ','))
+    {
+        readInvalidNumberToken(expression, currentPosition, tokenStartPosition, errors);
+        return true;
+    }
+    // Получить строковое представление числа
+    string numberText = expression.substr(tokenStartPosition, currentPosition - tokenStartPosition);
+    // Проверить, что модуль числа не превышает 2147483647
+    double numberValue = stod(numberText);
+    if (fabs(numberValue) > 2147483647.0)
+    {
+        errors.push_back({ ErrorType::NUMBER_OUT_OF_RANGE, tokenStartPosition, numberText });
+        return true;
+    }
+    // Добавить токен числа в список
+    tokens.push_back({ numberText, tokenStartPosition, TokenType::NUMBER });
+    return true;
+}
+
+bool readVariableToken(const string& expression, int& currentPosition, vector<Token>& tokens)
+{
+    // Если текущий символ не является латинской буквой, переменная не считывается
+    if ((unsigned char)expression[currentPosition] >= 128 ||
+        !isalpha((unsigned char)expression[currentPosition]))
+    {
+        return false;
+    }
+    // Запомнить позицию начала имени переменной
+    int tokenStartPosition = currentPosition;
+    // Считать имя переменной целиком
+    while (currentPosition < (int)expression.size() && (unsigned char)expression[currentPosition] < 128 && isalnum((unsigned char)expression[currentPosition]))
+    {
+        currentPosition++;
+    }
+    // Добавить токен переменной в список
+    tokens.push_back({ expression.substr(tokenStartPosition, currentPosition - tokenStartPosition), tokenStartPosition, TokenType::VARIABLE });
+    return true;
+}
+
+bool readOperatorToken(const string& expression, int& currentPosition, vector<Token>& tokens)
+{
+    // Если текущий символ не является оператором или скобкой
+    if (string("+-*/^()").find(expression[currentPosition]) == string::npos)
+    {
+        return false;
+    }
+    // Сохранить текст текущего символа
+    string tokenText(1, expression[currentPosition]);
+    // По умолчанию считать токен оператором
+    TokenType tokenType = TokenType::OPERATOR;
+    // Если символ является открывающей скобкой
+    if (expression[currentPosition] == '(')
+    {
+        tokenType = TokenType::LEFT_BRACKET;
+    }
+    // Если символ является закрывающей скобкой
+    else if (expression[currentPosition] == ')')
+    {
+        tokenType = TokenType::RIGHT_BRACKET;
+    }
+    // Если символ является минусом, определить: унарный он или бинарный
+    else if (expression[currentPosition] == '-')
+    {
+        bool isUnary = tokens.empty() || tokens.back().type == TokenType::LEFT_BRACKET || tokens.back().type == TokenType::OPERATOR;
+        tokenText = isUnary ? "~" : "-";
+    }
+    // Добавить соответствующий токен в список
+    tokens.push_back({ tokenText, currentPosition, tokenType });
+    // Перейти к следующему символу
+    currentPosition++;
+    return true;
+}
+
+void validateTokenSequence(const vector<Token>& tokens, vector<Error>& errors)
+{
     // После завершения прохода проверить токены
-    for (int j = 0; j < (int)t.size(); j++)
+    for (int tokenIndex = 0; tokenIndex < (int)tokens.size(); tokenIndex++)
     {
         // Определить, является ли текущий токен бинарным оператором
-        bool currentIsBinaryOperator = t[j].type == TokenType::OPERATOR && t[j].text != "~";
+        bool currentIsBinaryOperator = tokens[tokenIndex].type == TokenType::OPERATOR && tokens[tokenIndex].text != "~";
         // Если бинарный оператор стоит в начале выражения
-        if (j == 0 && currentIsBinaryOperator)
+        if (tokenIndex == 0 && currentIsBinaryOperator)
         {
-            errors.push_back({ ErrorType::MISSING_OPERAND, t[j].position, t[j].text });
+            errors.push_back({ ErrorType::MISSING_OPERAND, tokens[tokenIndex].position, tokens[tokenIndex].text });
         }
         // Если бинарный оператор стоит в конце выражения
-        if (j == (int)t.size() - 1 && currentIsBinaryOperator)
+        if (tokenIndex == (int)tokens.size() - 1 && currentIsBinaryOperator)
         {
-            errors.push_back({ ErrorType::MISSING_OPERAND, t[j].position, t[j].text });
+            errors.push_back({ ErrorType::MISSING_OPERAND, tokens[tokenIndex].position, tokens[tokenIndex].text });
         }
-        // Для проверки соседних токенов нужен предыдущий токен
-        if (j == 0) {
-            continue;
-        }
-        // Определить, является ли предыдущий токен операндом или закрывающей скобкой
-        bool previousIsOperand = t[j - 1].type == TokenType::NUMBER || t[j - 1].type == TokenType::VARIABLE || t[j - 1].type == TokenType::RIGHT_BRACKET;
-        // Определить, является ли текущий токен операндом или открывающей скобкой
-        bool currentIsOperandOrLeftBracket = t[j].type == TokenType::NUMBER || t[j].type == TokenType::VARIABLE || t[j].type == TokenType::LEFT_BRACKET;
-        // Если подряд идут два операнда, добавить ошибку MISSING_OPERATOR
-        // Если после операнда сразу идёт открывающая скобка, добавить ошибку MISSING_OPERATOR
-        // Если после закрывающей скобки сразу идёт операнд, добавить ошибку MISSING_OPERATOR
-        if (previousIsOperand && currentIsOperandOrLeftBracket)
+        // Проверка соседних токенов выполняется только если предыдущий токен существует
+        if (tokenIndex > 0)
         {
-            errors.push_back({ ErrorType::MISSING_OPERATOR, t[j].position, t[j].text });
-        }
-        // Определить, является ли предыдущий токен бинарным оператором
-        bool previousIsBinaryOperator = t[j - 1].type == TokenType::OPERATOR && t[j - 1].text != "~";
-        // Если подряд идут два бинарных оператора, добавить ошибку MISSING_OPERAND
-        if (previousIsBinaryOperator && currentIsBinaryOperator)
-        {
-            errors.push_back({ ErrorType::MISSING_OPERAND, t[j].position, t[j].text });
-        }
-        // Если после открывающей скобки идёт бинарный оператор
-        if (t[j - 1].type == TokenType::LEFT_BRACKET && currentIsBinaryOperator)
-        {
-            errors.push_back({ ErrorType::MISSING_OPERAND, t[j].position, t[j].text });
+            // Определить, является ли предыдущий токен операндом или закрывающей скобкой
+            bool previousIsOperand = tokens[tokenIndex - 1].type == TokenType::NUMBER || tokens[tokenIndex - 1].type == TokenType::VARIABLE || tokens[tokenIndex - 1].type == TokenType::RIGHT_BRACKET;
+            // Определить, является ли текущий токен операндом или открывающей скобкой
+            bool currentIsOperandOrLeftBracket = tokens[tokenIndex].type == TokenType::NUMBER || tokens[tokenIndex].type == TokenType::VARIABLE || tokens[tokenIndex].type == TokenType::LEFT_BRACKET;
+            // Если подряд идут два операнда или после операнда сразу идет открывающая скобка
+            if (previousIsOperand && currentIsOperandOrLeftBracket)
+            {
+                errors.push_back({ ErrorType::MISSING_OPERATOR, tokens[tokenIndex].position, tokens[tokenIndex].text });
+            }
+            // Определить, является ли предыдущий токен бинарным оператором
+            bool previousIsBinaryOperator = tokens[tokenIndex - 1].type == TokenType::OPERATOR && tokens[tokenIndex - 1].text != "~";
+            // Если подряд идут два бинарных оператора
+            if (previousIsBinaryOperator && currentIsBinaryOperator)
+            {
+                errors.push_back({ ErrorType::MISSING_OPERAND, tokens[tokenIndex].position, tokens[tokenIndex].text });
+            }
+            // Если после открывающей скобки идет бинарный оператор
+            if (tokens[tokenIndex - 1].type == TokenType::LEFT_BRACKET && currentIsBinaryOperator)
+            {
+                errors.push_back({ ErrorType::MISSING_OPERAND, tokens[tokenIndex].position, tokens[tokenIndex].text });
+            }
         }
     }
+}
+
+vector<Token> tokenize(const string& expression, vector<Error>& errors)
+{
+    // Создать пустой список токенов
+    vector<Token> tokens;
+    // Установить индекс текущего символа в начало строки
+    int currentPosition = 0;
+    // Пока индекс меньше длины строки
+    while (currentPosition < (int)expression.size())
+    {
+        // Если текущий символ - пробел, перейти к следующему символу
+        if (isspace((unsigned char)expression[currentPosition]))
+        {
+            currentPosition++;
+        }
+        // Если текущий символ - цифра, считать числовой токен
+        else if (isdigit((unsigned char)expression[currentPosition]))
+        {
+            readNumberToken(expression, currentPosition, tokens, errors);
+        }
+        // Если число начинается с точки или запятой, формат числа некорректен
+        else if (expression[currentPosition] == '.' || expression[currentPosition] == ',')
+        {
+            int tokenStartPosition = currentPosition;
+            readInvalidNumberToken(expression, currentPosition, tokenStartPosition, errors);
+        }
+        // Если текущий символ - латинская буква, считать имя переменной
+        else if ((unsigned char)expression[currentPosition] < 128 && isalpha((unsigned char)expression[currentPosition]))
+        {
+            readVariableToken(expression, currentPosition, tokens);
+        }
+        // Если текущий символ - оператор или скобка, считать соответствующий токен
+        else if (string("+-*/^()").find(expression[currentPosition]) != string::npos)
+        {
+            readOperatorToken(expression, currentPosition, tokens);
+        }
+        // Если текущий символ не относится ни к одному допустимому случаю
+        else
+        {
+            errors.push_back({ ErrorType::INVALID_SYMBOL, currentPosition, string(1, expression[currentPosition]) });
+            currentPosition++;
+        }
+    }
+    // Если при разборе символов не было ошибок, проверить последовательность токенов
+    if (errors.empty())
+    {
+        validateTokenSequence(tokens, errors);
+    }
     // Вернуть список токенов
-    return t;
+    return tokens;
 }
 
 vector<Token> toPostfix(const vector<Token>& tokens, vector<Error>& errors) {
